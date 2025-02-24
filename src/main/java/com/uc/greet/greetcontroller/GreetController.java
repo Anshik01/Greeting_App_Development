0e27a3c05c65c12979f817fbe25ca2756c80521d
Anshik Jaiswal <anshik0706@gmail.com>
2025-02-24 12:37:15 +0530

    [Anshik Jaiswal] Add. adding modified File

--- src/main/java/com/uc/greet/greetcontroller/GreetController.java
+++ src/main/java/com/uc/greet/greetcontroller/GreetController.java
@@ -2,30 +2,21 @@ package com.uc.greet.greetcontroller;
 
 import com.uc.greet.entity.Greeting;
 import com.uc.greet.entity.User;
+import com.uc.greet.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetController {
-    private static final String template = "Hello, %s!";
 
-    @GetMapping("/greet")
-    public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "World") String name){
-        return new Greeting(String.format(template, name));
-    }
+    // Autowire the GreetingService to use its methods
+    @Autowired
+    private GreetingService greetingService;
 
-    @PostMapping("/post")
-    public Greeting postGreeting(@RequestBody User user){
-        return new Greeting(String.format(template, user.getFirstName() + " " + user.getLastName() + "!"));
-    }
-
-    @PutMapping("/put/{firstName}")
-    public Greeting putGreeting(@PathVariable String firstName,
-                                @RequestParam(value = "lastName") String lastName){
-        return new Greeting(String.format(template, firstName + " " + lastName + "!"));
-    }
-
-    @DeleteMapping("/delete/{name}")
-    public Greeting deleteGreeting(@PathVariable String name){
-        return new Greeting(String.format("User name " + name + " Deleted successfully!"));
+    // Handles GET requests to /greeting/simple
+    @GetMapping("/hello")
+    public Greeting getSimpleHello(){
+        // Uses the service layer to get a simple greeting message
+        return greetingService.simpleGreeting();
     }
 }
